16ab161bbd2cecc70c8d64ed5e6deefd44126d7e
prabhkirank12 <prabhkirank2@gmail.com>
2020-12-29 20:45:08 -0500

    Clickable title

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import "./styles/index.scss";
 import dropdown from "./scripts/dropdown";
 import placeImage from "./scripts/image";
-// import Map from "./scripts/map";
+import modal from "./scripts/modal";
+import globe from "./scripts/map";
+import home from "./scripts/title";
+
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("dropdown-btn").addEventListener('click', dropdown);
     document.getElementById("dropdown").addEventListener('click', e => e.stopPropagation());
+    document.getElementById("play-bttn").addEventListener('click', globe);
+    document.getElementById("play-bttn").addEventListener('click', e => e.stopPropagation());
+    document.getElementById("title").addEventListener('click', home);
+    document.getElementById("title").addEventListener('click', e => e.stopPropagation());
+    document.getElementById
     document.getElementById(body);
     placeImage();
-    // Map();
+    modal();
+
 })
--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,16 +1,69 @@
-import dojo from "dojo";
-const Map = require("esri/Map");
-const MapView = require("esri/views/MapView");
- 
-export default function(){
-    var map = new Map({
-            basemap: "topo-vector"
-    });
 
-    var view = new MapView({
-            container: "viewDiv",
-        map: map,
-        center: [-118.71511,34.09042],
-        zoom: 11
-    });
-};
+function globe(){
+
+    document.getElementById("content").innerHTML = "";
+
+    am4core.ready(function() {
+    
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+    
+    var chart = am4core.create("chartdiv", am4maps.MapChart);
+    
+    // Set map definition
+    chart.geodata = am4geodata_worldLow;
+    
+    // Set projection
+    chart.projection = new am4maps.projections.Orthographic();
+    chart.panBehavior = "rotateLongLat";
+    chart.deltaLatitude = -20;
+    chart.padding(20,20,20,20);
+    
+    // limits vertical rotation
+    chart.adapter.add("deltaLatitude", function(delatLatitude){
+        return am4core.math.fitToRange(delatLatitude, -90, 90);
+    })
+    
+    // Create map polygon series
+    var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+    
+    // Make map load polygon (like country names) data from GeoJSON
+    polygonSeries.useGeodata = true;
+    
+    // Configure series
+    var polygonTemplate = polygonSeries.mapPolygons.template;
+    polygonTemplate.tooltipText = "{name}";
+    polygonTemplate.fill = am4core.color("pink");
+    polygonTemplate.stroke = am4core.color("#454a58");
+    polygonTemplate.strokeWidth = 1.5;
+    
+    var graticuleSeries = chart.series.push(new am4maps.GraticuleSeries());
+    graticuleSeries.mapLines.template.line.stroke = am4core.color("#ffffff");
+    graticuleSeries.mapLines.template.line.strokeOpacity = 0.08;
+    graticuleSeries.fitExtent = false;
+    
+    
+    chart.backgroundSeries.mapPolygons.template.polygon.fillOpacity = 0.1;
+    chart.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#ffffff");
+    
+    // Create hover state and set alternative fill color
+    var hs = polygonTemplate.states.create("hover");
+    hs.properties.fill = chart.colors.getIndex(0).brighten(-0.5);
+    
+    let animation;
+    setTimeout(function(){
+      animation = chart.animate({property:"deltaLongitude", to:100000}, 20000000);
+    }, 3000)
+    
+    chart.seriesContainer.events.on("down", function(){
+    if(animation){
+      animation.stop();
+    }
+    })
+    
+    }); // end am4core.ready()
+
+}   
+
+export default globe;
